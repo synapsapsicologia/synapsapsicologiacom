03c9abf166da0eed754afaae9bc701624b185537
Jaime Cabrera <jedu41@gmail.com>
2026-08-07 23:34:53 -0600

    fix: forzar redeploy para cargar variables de Upstash

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -471,3 +471,5 @@ export async function setDiasNoLaborables(fechas: string[]): Promise<void> {
     throw new Error("No se actualizaron las fechas porque la base de datos está inaccesible.");
   }
 }
+/ /   F o r z a r   r e d e p l o y  
+ 
